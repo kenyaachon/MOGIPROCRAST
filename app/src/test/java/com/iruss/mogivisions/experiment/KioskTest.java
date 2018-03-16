e961071a30be2e667e8d64efd8bb162b520b1838
Moses Mbugua <swahili1963@gmail.com>
2018-03-16 10:24:24 -0700

    Add files via upload

---
+++ app/src/test/java/com/iruss/mogivisions/experiment/KioskTest.java
@@ -0,0 +1,10 @@
+package com.iruss.mogivisions.experiment;
+
+/**
+ * Created by Moses on 3/16/2018.
+ */
+
+public class KioskTest {
+    //Test for making sure the phone app or phone activity is called properly
+    //Test for making sure the camera app is called properly
+}
---
+++ app/src/test/java/com/iruss/mogivisions/experiment/TriviaTest.java
@@ -0,0 +1,8 @@
+package com.iruss.mogivisions.experiment;
+
+/**
+ * Created by Moses on 3/16/2018.
+ */
+
+public class TriviaTest {
+}
