1c96f507b62f50053a96ded1bda96b379441b7b1
Moses Mbugua <swahili1963@gmail.com>
2018-03-16 10:18:48 -0700

    Add files via upload

---
+++ app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
@@ -0,0 +1,33 @@
+package com.iruss.mogivisions.experiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * Page where you choose whether or not to activate the challenge activity
+ */
+public class KioskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiosk);
+    }
+
+    /**
+     * Need a button for going to the Challenge_Activity
+     */
+
+    /**
+     * Need to have a timer for how long, the phone stays locked
+     *
+     *
+     */
+
+    /**
+     * For the necessary features of the phone,
+     * we need a phone button, that opens the phone for emergency calls
+     * we need a camera button, that opens the camera app
+     */
+}
