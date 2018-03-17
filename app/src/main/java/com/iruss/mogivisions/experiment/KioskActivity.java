10d1c42fc9017e1f6c86640f4a28cf45e0079532
Moses Mbugua <swahili1963@gmail.com>
2018-03-17 00:05:47 -0700

    Create Action when unlockPhone and Phone buttons are pressed

--- app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
+++ app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
@@ -1,7 +1,11 @@
 package com.iruss.mogivisions.experiment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -13,21 +17,72 @@ public class KioskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk);
+
+        response();
+        call();
     }
+
 
     /**
      * Need a button for going to the Challenge_Activity
+     * Reponse activated when user presses unlock phone button
      */
+    public void response(){
+        Button unlock = findViewById(R.id.unlockPhone);
 
-    /**
+        unlock.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                //Intent intent = new Intent(this, TriviaActivity.class);
+                //EditText editText = (EditText) findViewById(R.id.editText);
+                //String message = editText.getText().toString();
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                //startActivity(intent);
+                trivia();
+            }
+        });
+
+
+    }
+    //Calls the Trivia Activity
+    public void trivia(){
+        Intent intent = new Intent(this, TriviaActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+
+
+
+    /*
      * Need to have a timer for how long, the phone stays locked
      *
      *
      */
 
-    /**
+    /*
      * For the necessary features of the phone,
      * we need a phone button, that opens the phone for emergency calls
      * we need a camera button, that opens the camera app
      */
+
+    /**.
+     *When the phone button is pressed the user will able to do emergency calls
+     */
+    public void call(){
+        Button callApp = findViewById(R.id.phone);
+        callApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri number = Uri.parse("tel:5551234");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+            }
+        });
+    }
+
 }
