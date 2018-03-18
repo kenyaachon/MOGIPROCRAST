5c8b103cf839c0da1b7907ba2e59f5aae3070de7
Moses Mbugua <swahili1963@gmail.com>
2018-03-18 00:02:56 -0700

    Get Camera permission from User

--- app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
+++ app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
@@ -1,7 +1,11 @@
 package com.iruss.mogivisions.experiment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -49,9 +53,6 @@ public class KioskActivity extends AppCompatActivity {
     //Calls the Trivia Activity
     public void trivia(){
         Intent intent = new Intent(this, TriviaActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
@@ -94,12 +95,50 @@ public class KioskActivity extends AppCompatActivity {
         cameraApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent cameraIntent = new Intent("android.media.action.IMAGE_CAPTURE");
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(cameraIntent);
+                 if ( cameraCheck()) {
+                     Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                     startActivity(cameraIntent);
+                 }
             }
         });
     }
 
+    /**
+     * Checks if app has user permission to device camera
+     * returns true if Permission to use Camera is allowed
+     */
+    int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    public boolean cameraCheck(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+                return true;
+            }
+        } else {
+            // Permission has already been granted
+            return true;
+        }
+        return false;
+    }
 
 }
