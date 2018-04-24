10cc419f155a1c5eec3b3bf91534fad41c4bf1c4
Moses Mbugua <swahili1963@gmail.com>
2018-04-23 22:38:46 -0700

    Kiosk Mode implemented

--- app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
+++ app/src/main/java/com/iruss/mogivisions/experiment/KioskActivity.java
@@ -6,24 +6,58 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.iruss.mogivisions.kiosk.*;
 
 
 /**
  * Page where you choose whether or not to activate the challenge activity
  */
 public class KioskActivity extends AppCompatActivity {
+    private final List blockedKeys = new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP));
+    private Button hiddenExitButton;
+    private TextView timeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
         setContentView(R.layout.activity_kiosk);
+
+
+
+        // every time someone enters the kiosk mode, set the flag true
+        PrefUtils.setKioskModeActive(true, getApplicationContext());
+
+        hiddenExitButton = (Button) findViewById(R.id.exitButton);
+        hiddenExitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Break out!
+                PrefUtils.setKioskModeActive(true, getApplicationContext());
+                Toast.makeText(getApplicationContext(),"You can leave the app now!", Toast.LENGTH_SHORT).show();
+                PackageManager pm = getPackageManager();
+                pm.clearPackagePreferredActivities ("de.example.android.kiosk");
+            }
+        });
+
+
+        timeView = findViewById(R.id.timeView);
+        unlockPhone(2);
 
         response();
         call();
@@ -31,16 +65,59 @@ public class KioskActivity extends AppCompatActivity {
         settings();
     }
 
+    public void unlockPhone(int hours){
+        int time = hours * 3600000;
+
+        //Delays the reveal of the exit button
+        new CountDownTimer(20000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timeView.setText("Time remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                hiddenExitButton.setVisibility(View.VISIBLE);
+            }
+        }.start();
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(!hasFocus) {
+            // Close every kind of system dialog
+            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // nothing to do here
+        // … really
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (blockedKeys.contains(event.getKeyCode())) {
+            return true;
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
 
         // Update text about lockout time
-        TextView textView = findViewById(R.id.textView);
+        timeView = findViewById(R.id.timeView);
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         int hours = Integer.parseInt(sharedPref.getString("lockout_time", "12"));
-        textView.setText("Unlock time: " + hours + " hours");
+        timeView.setText("Unlock time: " + hours + " hours");
     }
 
     /**
@@ -169,5 +246,6 @@ public class KioskActivity extends AppCompatActivity {
         }
         return false;
     }
+
 
 }
