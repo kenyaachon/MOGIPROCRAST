822a6841ec2b041ee9fd1dcf0a22d77296776739
Moses Mbugua <swahili1963@gmail.com>
2018-04-28 15:45:51 -0700

    Adjusted Experiment package structure

--- app/src/main/java/com/iruss/mogivisions/experiment/HomeActivity.java
+++ app/src/main/java/com/iruss/mogivisions/experiment/HomeActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.iruss.mogivisions.kiosk.KioskActivity;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
