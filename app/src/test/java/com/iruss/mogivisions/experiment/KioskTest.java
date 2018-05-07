1e7fcbe91e4a78dc8abe6300641b0ed2481bc9fb
Moses Mbugua <swahili1963@gmail.com>
2018-05-06 22:52:09 -0700

    Put back KioskTest and TriviaTest after merge

--- app/src/test/java/com/iruss/mogivisions/experiment/KioskTest.java
+++ app/src/test/java/com/iruss/mogivisions/experiment/KioskTest.java
@@ -7,11 +7,29 @@ package com.iruss.mogivisions.experiment;
 
 
 
+import com.iruss.mogivisions.kiosk.KioskActivity;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 // GET THIS TO ACTUALLY PUSH PROPERLY
 // ALSO EVERY TIME I TRY TO RUN : Error running 'TriviaTest': The activity 'TriviaTest' is not declared in AndroidManifest.xml
 // THIS HAPPENS FOR ALL TESTS
-public class KioskTest  {
+public class KioskTest extends KioskActivity {
+    //Test for making sure the phone app or phone activity is called properly
+    //Test for making sure the camera app is called properly
 
+    @Test
+    public void cameraPermissionCheckTest() {
+
+        // cameraCheck should return false to begin with because presumably permission hasn't been asked for
+        assertEquals(false, cameraCheck());
+
+        // do something here to give permission to make cameraCheck return true
+        // Assert.assertEquals(cameraCheck(), true);
+
+    }
 
 
 
