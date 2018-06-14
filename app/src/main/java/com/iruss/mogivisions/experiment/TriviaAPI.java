30d711366619344a558cd916131e8fad422f0f67
Moses Mbugua <swahili1963@gmail.com>
2018-06-14 14:17:49 -0700

    Copyright for OPEN TRIVIA DATABASE

--- app/src/main/java/com/iruss/mogivisions/experiment/TriviaAPI.java
+++ app/src/main/java/com/iruss/mogivisions/experiment/TriviaAPI.java
@@ -4,6 +4,23 @@ package com.iruss.mogivisions.experiment;
  * Created by Moses on 3/18/2018.
  */
 
+/*
+ * Copyright (C) Open Trivia Database
+ *
+ * Licensed under the Creative Commons Attribution-ShareAlike 4.0 International License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://creativecommons.org/licenses/by-sa/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
